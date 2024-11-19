4e8f0614b441d7fe993ff5ace3c8530f23cd1ff8
anudeep749 <anudeep.n@ikonicit.com>
2024-11-19 12:52:01 +0530

    error

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/about/About.jsx";
-import Services from "./components/services/Services.jsx";
+import Services from "./components/services/Services.jsx"; 
 // import Footer from "./components/footer/Footer.jsx";
-import Testimonials from "./components/Testimonials/Testimonials.jsx";
-import Contacts from "./components/Contacts/Contacts.jsx";
+import Testimonials from "./components/testimonials/Testimonials.jsx";
+import Contacts from "./components/contacts/Contacts.jsx";
 
 const App = () => {
   return (
@@ -16,6 +16,7 @@ const App = () => {
       <Testimonials />
       <Services />
       <Contacts />
+      {/* <Footer /> */}
     </div>
   );
 };
